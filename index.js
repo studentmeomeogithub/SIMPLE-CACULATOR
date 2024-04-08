3e607c6ff9e2a647f253239259635591b6d24eed
studentmeomeogithub <arsalanameomeo9@gmail.com>
2024-04-09 00:17:48 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+#! /usr/bin/env node 
+//uper line is called shabang
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    { message: "Enter your first number", type: "number", name: "firstnumber" },
+    { message: "Enter your secend number", type: "number", name: "secondnumber" },
+    {
+        message: "Select ono Of the operators to perform operation",
+        type: "list",
+        name: "operator",
+        choices: ["Addition", "Subtraction", "Multiplication", "Division"],
+    },
+]);
+if (answer.operator === "Addition") {
+    console.log(answer.firstnumber + answer.secondnumber);
+}
+else if (answer.operator === "Subtraction") {
+    console.log(answer.firstnumber - answer.secondnumber);
+}
+else if (answer.operator === "Multiplication") {
+    console.log(answer.firstnumber * answer.secondnumber);
+}
+else if (answer.operator === "Division") {
+    console.log(answer.firstnumber / answer.secondnumber);
+    ;
+}
+else {
+    console.log("Please select valid number");
+}
